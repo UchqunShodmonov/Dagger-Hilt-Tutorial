f1ff116270adcae7c6a1328ce3e73a4f4046d9b4
uchqun <shodmonov.uchqun@gmail.com>
2021-01-01 11:29:12 +0500

    Initial commit

---
+++ app/src/main/java/uz/uchqun/hilttutorial/MainModule.kt
@@ -0,0 +1,24 @@
+package uz.uchqun.hilttutorial
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ActivityScoped
+import javax.inject.Named
+
+@Module
+@InstallIn(ActivityComponent::class)
+object MainModule {
+
+    @ActivityScoped
+    @Provides
+    @Named("String2")
+    fun provideTestString2(
+        @ApplicationContext context: Context,
+        @Named("String1") textString1: String
+    ) = "${context.getString(R.string.sting_to_inject)} - $textString1"
+
+}
